41a1c65823c47ec43506f841fa5254ddce90318a
Dennis Sammy <dennissammy77@gmail.com>
2024-06-06 23:45:20 +0300

    Changws

--- tutorial/OdiBet/test.js
+++ tutorial/OdiBet/test.js
@@ -4,18 +4,18 @@ import cheerio from 'cheerio';
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  const url = 'https://spribe.co/games/aviator';
+  const url = 'https://1xbet.co.ke/allgamesentrance/crash';
 
   try {
     await page.goto(url);
 
     // Click on elements
-    let html = await page.evaluate(()=> document.body.innerHTML);
+s   let html = await page.evaluate(()=> document.body.innerHTML);
     const $ = cheerio.load(html);
-
-    const event = await page.$('a.demo-link');
-    await event.evaluate( event => event.click() );
-
+	
+	
+	console.log($('p.crash-history__title'));
+	/**
     const eventmodal = await page.$('button.btn.btn-md.btn-primary.btn-age.mt-3.mt-lg-0');
     await eventmodal.evaluate( eventmodal => eventmodal.click() );
 
@@ -37,6 +37,7 @@ import cheerio from 'cheerio';
 
     // Track data changes every 5 seconds (adjust interval as needed)
     setInterval(trackDataChanges, 5000);
+	 **/
   } catch (error) {
     console.error('An error occurred:', error);
   } finally {
