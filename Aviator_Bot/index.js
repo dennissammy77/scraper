a043f85e51a58a1ecedb95968a339dffe3c3be88
Dennis Sammy <dennissammy77@gmail.com>
2024-06-10 21:48:56 +0300

    Changws

--- Aviator_Bot/index.js
+++ Aviator_Bot/index.js
@@ -1,93 +1,48 @@
 import puppeteer, { Puppeteer} from "puppeteer";
 import cheerio from "cheerio"; // 
 import { select, filter, is, some } from "cheerio-select";
+//import { once } from 'events';
 
 async function main(){
 	try{
 		const browser = await puppeteer.launch({headless:false});
 		
-		const url = 'https://spribe.co/games/aviator'
+		const url = 'https://www.betika.com/en-ke/aviator'
 
 		const page = await browser.newPage();
 		await page.goto(url,{timeout:0})
 			
-		const event = await page.$('a.demo.link');
+		const event = await page.$('button.account__payments__submit.button.button__secondary.purple');
 		await event?.evaluate(event => event.click());
 
-		const eventmodal = await page.$('button.btn.btn-primary.btn-lg.px-5.btn-demo.btn-danger');
+		const newPage = await page.waitForSelector('div.header.h-100.d-flex.justify-content-between.align-items-center')
 
-		await eventmodal?.evaluate(eventmodal => eventmodal.click());
 
-		const event_modal_2 = await page.$('button.btn.btn-md.btn-primary.btn-age.mt-3.mt-lg-0');
-		await event_modal_2?.evaluate(event_modal_2 => event_modal_2.click());
-
-		//const link = await page.$('a[href]');
-		
-		const R_fuc = ()=>{
-			const newTarget = await browser.waitForTarget(target =>
-				target.opener() === page.target()
-			);
-
-			const newPage = await newTarget.page();
-			console.log('Page Created!')
-			
-			let html = await target_page.evaluate(()=> document.body.innerHTML)
-			console.log(html);
-
-		}
-		const [target] = await Promise.all([
-			new Promise(resolve => browser.once('targetcreated', resolve)),
-			 R_fuc() 
-			link.click({ button: 'middle' }),
-		]);
-		/**
-		const target_page = await target.page();
-		
-		await target_page.bringToFront();
-
-		//const target_page = await browser.waitForTarget(target => target.url() === 'https://aviator-demo.spribegaming.com/');
-		let html = await target_page.evaluate(()=> document.body.innerHTML)
+		let html = await newPage.evaluate(() => document.body.innerHTML);
 		console.log(html);
-/**
-		const newPage_ = await target.page();
-		
-		await newPage_.bringToFront();
-		
-		let html = await newPage_.evaluate(() => document.body.innerHTML);
-		console.log(html)
-		//const newPage = await target.page();
-		//await newPage.bringToFront();
-		//await page.waitForFunction("window.location.hostname == 'https://aviator-demo.spribegaming.com/'")
 
-		//let html = await newPage.evaluate(()=> document.body.innerHTML);
-		//console.log(html);
-
-		//let $ = cheerio.load(html);
-
-		//console.log(await page.$$eval('div > div', el => el.map(i => i.textContent)));
-		
-		
-		//const url = 'https://spribe.co/games/aviator'
-		//const url_1 = 'https://aviator-demo.spribegaming.com/?currency=USD&operator=demo&jurisdiction=CW&lang=EN&return_url=https:%2F%2Fspribe.co%2Fgames&user=95234&token=cjf19MhDJPVyXPlQ3buTd9ZP0cbH0H3L'
 		/**
-		let html = await page.evaluate(()=> document.body.innerHTML);
-		let $ = cheerio.load(html);
-
-		const event = await page.$('a.demo.link');
-		await event?.evaluate(event => event.click());
-
 		const eventmodal = await page.$('button.btn.btn-primary.btn-lg.px-5.btn-demo.btn-danger');
+
+		const newPagePromise = new Promise(x => browser.once('targetcreated', target => x(target.page())));
+
 		await eventmodal?.evaluate(eventmodal => eventmodal.click());
 
 		const event_modal_2 = await page.$('button.btn.btn-md.btn-primary.btn-age.mt-3.mt-lg-0');
+		
 		await event_modal_2?.evaluate(event_modal_2 => event_modal_2.click());
 
-		page.once('load', () => console.log('Page loaded!'));
+		//const newPagePromise = browser.on('targetcreated', function(){
+		//	console.log('New Tab Created');
+		//})
+		const newPage = await newPagePromise;
 
-		//	console.log(await $('#_ngcontent-ibv-c47 span.currency'))
-//
-//	**/
-		page.once('load', () => console.log('Page loaded!'));
+		//let html = await newPage.evaluate(()=>document.body.innerHTML);
+		//console.log(html)
+
+		console.log((await browser.pages()).length);
+		console.log((await newPage.content()));
+		 **/
 		console.log('******Waiting*****');
 	
 	}catch(err){
