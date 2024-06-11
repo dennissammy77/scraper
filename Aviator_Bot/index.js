05adf2fb6f92adaa667780bd2cb0ed3296dd4304
Dennis Sammy <dennissammy77@gmail.com>
2024-06-11 23:02:17 +0300

    Changws

--- Aviator_Bot/index.js
+++ Aviator_Bot/index.js
@@ -21,28 +21,6 @@ async function main(){
 		let html = await newPage.evaluate(() => document.body.innerHTML);
 		console.log(html);
 
-		/**
-		const eventmodal = await page.$('button.btn.btn-primary.btn-lg.px-5.btn-demo.btn-danger');
-
-		const newPagePromise = new Promise(x => browser.once('targetcreated', target => x(target.page())));
-
-		await eventmodal?.evaluate(eventmodal => eventmodal.click());
-
-		const event_modal_2 = await page.$('button.btn.btn-md.btn-primary.btn-age.mt-3.mt-lg-0');
-		
-		await event_modal_2?.evaluate(event_modal_2 => event_modal_2.click());
-
-		//const newPagePromise = browser.on('targetcreated', function(){
-		//	console.log('New Tab Created');
-		//})
-		const newPage = await newPagePromise;
-
-		//let html = await newPage.evaluate(()=>document.body.innerHTML);
-		//console.log(html)
-
-		console.log((await browser.pages()).length);
-		console.log((await newPage.content()));
-		 **/
 		console.log('******Waiting*****');
 	
 	}catch(err){
