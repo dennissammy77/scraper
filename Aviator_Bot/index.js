65cf314cf3a7b46b38dccfaa91990a7d64e4831d
Dennis Sammy <dennissammy77@gmail.com>
2024-08-27 03:33:21 +0300

    Clean up & Logging bet history

--- Aviator_Bot/index.js
+++ Aviator_Bot/index.js
@@ -2,98 +2,102 @@ import puppeteer, { Puppeteer} from "puppeteer";
 import cheerio from "cheerio"; // 
 import { select, filter, is, some } from "cheerio-select";
 //import { once } from 'events';
-
+import moment from 'moment'
 async function main(){
 	try{
+		// Launch Bot
 		const page = await Launcher();	
 		const url = 'https://www.betika.com/en-ke/aviator'
 		await page.goto(url,{timeout:0})
 		
-		const event = await page.$('button.account__payments__submit.button.button__secondary.purple');
-		await event?.evaluate(event => event.click());
-
-		let BetState = false;	
-		// Find the iframe using its ID
-		// Wait for the iframe to be available in the DOM
-  
+		// Initialize the game from menu
+		INITIALIZE(page);
 		await page.waitForSelector('#aviator-iframe');
 		const iframeElementHandle = await page.$('#aviator-iframe');
-		
-		if (iframeElementHandle) {
-    
-			const iframe = await iframeElementHandle.contentFrame();
-        
-			if (iframe) {
-
-				GET_WALLET_AMOUNT(iframe);
-				SET_AUTO_BET(iframe);
-				SET_AUTO_CASHOUT(iframe);
-				SET_AUTO_CASHOUT_VALUE(iframe);
-				
-				async function HANDLE_BUTTON_BET_STATE(){
-
-					const bet_selector_value = 'body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.first-row.auto-game-feature > div.buttons-block > button'
-					const cancel_bet_selector_value = '.btn.btn-danger.bet'
-					const bet_label_selector_value = 'body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.first-row.auto-game-feature.auto-game > div.buttons-block > button > span > label.label.text-uppercase'
-					const curr_stake_value_selector_value = 'body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.second-row > div.cashout-block > div.cashout-spinner-wrapper > div > app-spinner > div > div.input.full-width > input'
-					
-					await iframe.waitForSelector(curr_stake_value_selector_value);
-					const fieldHandle = await iframe.$(curr_stake_value_selector_value);
-					let fieldValue = await iframe.evaluate(x => x.value, fieldHandle);
-					console.log(fieldValue)
-
-					if (fieldValue !== '1.15'){
-						SET_AUTO_CASHOUT_VALUE(iframe)
-						return;
-					}
-
-					await iframe.waitForSelector(bet_selector_value, cancel_bet_selector_value,  { timeout: 600000 });
-
-//					await iframe.waitForSelector(cancel_bet_selector_value, { timeout: 600000 });
-
-
-					// Locate the specific element within the iframe
-					const BetElementHandle = await iframe.$(bet_selector_value, { timeout: 600000 });      
-					const CancelElementHandle = await iframe.$(cancel_bet_selector_value, { timeout: 600000 });  
-					if (CancelElementHandle){
-						console.log('betting');
-						//return false;
-						return;
-					}
-					GET_WALLET_AMOUNT(iframe)
-
-					await iframe.waitForSelector(bet_label_selector_value)
-					let betLabel = '';
-					const betLabelHandle = await iframe.$(bet_label_selector_value);      
-					if (betLabelHandle) {
-						// Extract the content of the element
-						betLabel = await iframe.evaluate(el => el.textContent.trim(), betLabelHandle);
-					} 
-					console.log(betLabel)
-					
-					if ( betLabel === 'Bet' ) {
-						BetElementHandle?.evaluate(event => event.click());
-						console.log('Bet clicked')
-					}
-					
-					return;
-				}
-				setInterval(
-					HANDLE_BUTTON_BET_STATE,
-					1500
-				);
-				//let state = true;
-				/*
-				while (!BetState) {
-					START_BET(iframe);				
-					state = false
-				}
-				*/
-			}
-		}
 
 		console.log('******Waiting*****');
+
+		// IFRAME Set up
+		if (!iframeElementHandle) {
+			throw new Error('IFrame Handler could not be identified');
+		};
 		
+		const iframe = await iframeElementHandle.contentFrame();
+		if (!iframe){
+			throw new Error('IFrame  could not be identified');
+		}
+		// SETUP PARAMETERS: Auto bet , Auto CashOut, Prediction Value, 
+		// BET HISTORY 
+
+		let BET_HISTORY_ARRAY = [];
+		const starting_wallet_starting_amount = await GET_WALLET_AMOUNT(iframe);
+		const starting_time = moment(Date.now()).format('h:mm:ss');;
+		let elapsed_time, wallet_amount_after_bet, wallet_amount_before_bet, bet_amount_placed=10, prediction=1.15, gain, cashout;
+
+		SETUP(iframe,JSON.stringify(prediction))
+		// INITIALIZE BOT
+		async function HANDLE_BUTTON_BET_STATE(){
+			// Verify Prediction has been correctly placed
+			const PREDICTION_VALUE = await PREDICTION_VERIFIER(iframe)
+			
+			if ( PREDICTION_VALUE !== JSON.stringify(prediction)){
+				SET_AUTO_CASHOUT_VALUE(iframe,JSON.stringify(prediction))
+				return;
+			}
+
+			// Verify Button State
+			const BETTING_STATE = await BET_BUTTON_STATE(iframe)
+
+			if (!BETTING_STATE){
+				return;
+			}
+
+			// SET amount before bet
+			wallet_amount_before_bet = await GET_WALLET_AMOUNT(iframe);
+
+			// Track button state
+			const BETTNG_STATE_TRACKER = await BET_BUTTON_STATE_TRACKER(iframe);
+
+			if ( BETTNG_STATE_TRACKER ){
+				BETTING_STATE?.evaluate(event => event.click());
+				// Set amount after bet
+				wallet_amount_after_bet = await GET_WALLET_AMOUNT(iframe);
+
+				// Track bets
+				if ( wallet_amount_after_bet > wallet_amount_before_bet ){
+					cashout = bet_amount_placed * parseFloat(prediction); // bet won
+				}
+				if ( wallet_amount_after_bet === wallet_amount_before_bet ){
+					return; // Do not push into bet history array
+				}
+				if ( wallet_amount_after_bet < wallet_amount_before_bet ){
+					cashout = 0; // bet lost
+				}
+				gain = wallet_amount_after_bet - starting_wallet_starting_amount; // profit / loss tracker
+
+				// Push into history array
+				BET_HISTORY_ARRAY.push({
+					starting_time:		starting_time,
+					time:				moment(Date.now()).format('h:mm:ss'),
+					starting_amount:	starting_wallet_starting_amount,
+					initial_amount:		wallet_amount_before_bet,
+					final_amount:		wallet_amount_after_bet,
+					stake:				bet_amount_placed,
+					multiplier:			prediction,
+					cashout:			cashout,
+					p_l:				cashout - bet_amount_placed,
+					gain:				gain
+
+				})
+				console.table(BET_HISTORY_ARRAY)
+			}
+		}
+		// RUN after 1.5 sec
+		setInterval(
+			HANDLE_BUTTON_BET_STATE,
+			1500
+		);
+
 	}catch(err){
 		console.error(err)
 	}
@@ -105,6 +109,20 @@ async function Launcher(){
 	return page;
 };
 
+async function INITIALIZE(page){
+	const event = await page.$('button.account__payments__submit.button.button__secondary.purple');
+	await event?.evaluate(event => event.click());
+	
+	return
+}
+
+async function SETUP(iframe,prediction){
+	GET_WALLET_AMOUNT(iframe);
+	SET_AUTO_BET(iframe);
+	SET_AUTO_CASHOUT(iframe);
+	SET_AUTO_CASHOUT_VALUE(iframe,prediction);
+}
+
 async function GET_WALLET_AMOUNT(iframe){
 	const selector_value = 'body > app-root > app-game > div > div.main-container > div.main-header > app-header > div > div.second-block.d-flex > div > div.balance.px-2.d-flex.justify-content-end.align-items-center > div > span.amount.font-weight-bold'
 
@@ -115,6 +133,7 @@ async function GET_WALLET_AMOUNT(iframe){
 		// Extract the content of the element
 		const spanText = await iframe.evaluate(el => el.textContent.trim(), ElementHandle);
 		console.log('Wallet Ammount:', spanText);
+		return parseFloat(spanText)
 	} else {
 		console.log('Element not found within the iframe.');
 	}
@@ -156,9 +175,9 @@ async function SET_AUTO_CASHOUT(iframe){
 	}
 }
 
-async function SET_AUTO_CASHOUT_VALUE(iframe){
+async function SET_AUTO_CASHOUT_VALUE(iframe,prediction){
 	const selector_value = 'body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.second-row > div.cashout-block > div.cashout-spinner-wrapper > div > app-spinner > div > div.input.full-width > input'
-	let cashout_value = '1.15';
+	let cashout_value = prediction;
 
 	await iframe.waitForSelector(selector_value);     
 	
@@ -177,35 +196,59 @@ async function SET_AUTO_CASHOUT_VALUE(iframe){
 	}
 }
 
-async function EXTRACT_MY_BETS(iframe){
-	const parent_selector_value = 'body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.info-board.pt-2 > app-bets-widget > div > app-my-bets-tab > div.h-100.scroll-y';
-	const child_selector_value = 'body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.info-board.pt-2 > app-bets-widget > div > app-my-bets-tab > div.h-100.scroll-y > app-bet-item > div';
+async function PREDICTION_VERIFIER(iframe){
+	const element_selector = 'body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.second-row > div.cashout-block > div.cashout-spinner-wrapper > div > app-spinner > div > div.input.full-width > input'
 	
-	let extractedBets = []; // Initialize an empty array to store extracted bets
+	await iframe.waitForSelector(element_selector);
+	const INPUTFIELD_HANDLER = await iframe.$(element_selector);
+	
+	if (INPUTFIELD_HANDLER) {
+		let INPUTFIELD_VALUE = await iframe.evaluate(input => input.value, INPUTFIELD_HANDLER);
+		console.log(INPUTFIELD_VALUE);
+		return INPUTFIELD_VALUE;
+	} else {
+		throw new Error('Element not found within the iframe.');
+		return null;
+	}
+}
 
-	await iframe.waitForSelector('div.h-100.scroll-y');   
+async function BET_BUTTON_STATE(iframe){
+	const bet_element_selector = 'body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.first-row.auto-game-feature > div.buttons-block > button'
+	const cancel_element_selector = '.btn.btn-danger.bet'
+	
+	await iframe.waitForSelector(bet_element_selector, cancel_element_selector,  { timeout: 600000 });
+	
+	// Locate the specific element within the iframe
+	
+	const BET_STATE_HANDLER = await iframe.$(bet_element_selector, { timeout: 600000 });
+	const CANCEL_BET_STATE_HANDLER = await iframe.$(cancel_element_selector, { timeout: 600000 }); 
+	
+	if (CANCEL_BET_STATE_HANDLER){
+		console.log('bet in progress');
+		return false;
+	}
+	if (BET_STATE_HANDLER){
+		return BET_STATE_HANDLER
+	}
+}
 
-	const parentElement = iframe.querySelector('div.h-100.scroll-y');
-    const betItems = parentElement.querySelectorAll('app-bet-item');
-/*
-	const parentElementHandle = await iframe.querySelector(parent_selector_value);
-	const childElementsHandle = await parentElementHandle.querySelectorAll('app-bet-item');
-	*/
-	betItems.forEach((betItem)=>{
-		const time = betItem.querySelector('.date > div')?.textContent.trim() || '';
-		const betAmount = betItem.querySelector('.bet .ng-star-inserted')?.textContent.trim() || '';
-		const multiplier = betItem.querySelector('.multiplier-block .bubble-multiplier')?.textContent.trim() || '';
-		const cashOut = betItem.querySelector('.cash-out')?.textContent.trim() || '';
+async function BET_BUTTON_STATE_TRACKER(iframe){
+	const element_selector = 'body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.first-row.auto-game-feature.auto-game > div.buttons-block > button > span > label.label.text-uppercase';
 
-		// Store the extracted data in an object
-		extractedBets.push({
-			time,
-			betAmount,
-	        multiplier,
-		    cashOut
-		});
-	})
-	console.log(extractedBets)
+	await iframe.waitForSelector(element_selector);
+	let Label = '';
+	const LABEL_HANDLER = await iframe.$(element_selector);
+
+	if (LABEL_HANDLER){
+		// Extract the content of the element
+		Label = await iframe.evaluate(el => el.textContent.trim(), LABEL_HANDLER);
+		if ( Label === 'Bet' ) {
+			return true;
+		}
+		return false;
+	} else {
+		throw new Error('Element not found within the iframe.');
+	}
 }
 
 main()
